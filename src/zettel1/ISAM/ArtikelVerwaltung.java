fea035ea4552d32c615b688a50fc9996fea5fe87
Marvin Kirsch <m-1234@live.de>
2019-01-16 23:36:32 +0100

    test commit

--- src/zettel1/ISAM/ArtikelVerwaltung.java
+++ src/zettel1/ISAM/ArtikelVerwaltung.java
@@ -6,66 +6,67 @@ package zettel1.ISAM;
 import java.io.*;
 import java.util.*;
 
-public class ArtikelVerwaltung{ 
+public class ArtikelVerwaltung {
 
-    List <Artikel> artikel = new ArrayList<Artikel>();    
-    List <Di> isam = new ArrayList<Di>();  
-    
+    List<Artikel> artikel = new ArrayList<>();
+    List<Di> isam = new ArrayList<>();
+
     //Methoden
     public void schreiben(int artnr, String artbez, double preis, String mge, String kuehl){
         try{
-            
+
             FileWriter fileW = new FileWriter("ARTIKEL.CSV",true);
-            
-            PrintWriter pw = new PrintWriter(fileW);     
-            
-            pw.print(artnr); 
-            pw.print(';'); 
+
+            PrintWriter pw = new PrintWriter(fileW);
+
+            pw.print(artnr);
+            pw.print(';');
             pw.print(artbez);
             pw.print(';');
             pw.print(preis);
             pw.print(';');
             pw.print(mge);
             pw.print(';');
-          
             pw.print(kuehl+System.getProperty("line.separator"));
-    
+
             pw.close();
-            
+
         } catch(IOException ioex){
             System.out.println("Es ist folgender IOExpeptionFehler aufgetreten: "+ioex.getMessage());
-        } 
-        
+        }
+
     }
 
     public void indexVerwaltung(){
         try{
-            
+
             RandomAccessFile file = new RandomAccessFile("ARTIKEL.CSV","r");
             long offset = 0;
-            
+
             offset = file.getFilePointer();
-            artikel.clear();      
+            artikel.clear();
             isam.clear();
-            
-            for(String line; (line = file.readLine()) != null; offset = file.getFilePointer()){  
-               
+
+            String line;
+            while ((line = file.readLine()) != null) {
+
                 Di obj = new Di();
-              
-                String[] spalte = line.split(";");  
-           
+
+                String[] split = line.split(";");
+
                 obj.offset = offset;
-                obj.artnr = Integer.parseInt(spalte[0]);
-               
+                obj.artnr = Integer.parseInt(split[0]);
+
                 isam.add(obj);
                 Artikel eingabe = new Artikel();
-                eingabe.artnr = Integer.parseInt(spalte[0]);
-                eingabe.artbez = spalte[1];
-                eingabe.preis = Double.parseDouble(spalte[2]);
-                eingabe.mge = spalte[3];
-                eingabe.kuehl = spalte[4];
-                
+                eingabe.artnr = Integer.parseInt(split[0]);
+                eingabe.artbez = split[1];
+                eingabe.preis = Double.parseDouble(split[2]);
+                eingabe.mge = split[3];
+                eingabe.kuehl = split[4];
+
                 artikel.add(eingabe);
+                offset = file.getFilePointer();
             }
             file.close();
         } catch(IOException ioex){
@@ -76,17 +77,17 @@ public class ArtikelVerwaltung{
     public void indexSchreiben(){
         Di obj = new Di();
         try{
-            
+
             FileWriter fw = new FileWriter("ARTIKEL.IDX",false);
-            fw.write("OFFSET; ARTNR "+System.getProperty("line.separator"));  
+            fw.write("OFFSET; ARTNR "+System.getProperty("line.separator"));
             for (int i = 0;i < isam.size();i++) {
                 obj = isam.get(i);
                 fw.write(""+obj.offset+";"+obj.artnr+System.getProperty("line.separator"));
-            } 
+            }
             fw.close();
         } catch(IOException ioex){
             System.out.println("Es ist folgender IOExpeptionFehler aufgetreten: "+ioex.getMessage());
-        }                       
+        }
     }
 
     public void ausgabe(){
@@ -97,28 +98,28 @@ public class ArtikelVerwaltung{
             Artikel art = new Artikel();
             art = artikel.get(i);
             System.out.println(art.artnr+";"+art.artbez+";"+art.preis+";"+art.mge +";"+art.kuehl);
-        }  
+        }
     }
 
     public void indexAusgabe(){
-        
+
         System.out.println("");
         System.out.println("Offset; Artnr");
-        System.out.println("------------------");   
+        System.out.println("------------------");
         for (int i = 0; i <= isam.size(); i++){
             Di di = new Di();
             di = isam.get(i);
             System.out.println(di.offset+"; "+di.artnr);
-        }  
+        }
     }
 
     public int seek(int suchnr){
         Di di = new Di();
         for (int i = 0; i<artikel.size(); i++){
             long offset;
-           
+
             di = isam.get(i);
-            
+
             if (di.artnr == suchnr) {
                 offset = di.offset;
                 try{
@@ -129,16 +130,16 @@ public class ArtikelVerwaltung{
                     System.out.println("Folgender Artikel wurde gefunden: ");
                     System.out.println("");
                     System.out.println("ARTNR;ARTBEZ;PREIS;MGE;KUEHL");
-        
-               System.out.println(file.readLine());
-                    
+
+                    System.out.println(file.readLine());
+
                     file.close();
                 } catch(IOException ioex){
-                System.out.println("Es ist ein Fehler aufgetreten: "+ioex.getMessage());
-                }    
+                    System.out.println("Es ist ein Fehler aufgetreten: "+ioex.getMessage());
+                }
                 return 1;
-            } 
-        } 
+            }
+        }
         return -1;
-    }  
+    }
 } 
